330db923a81a207b21b7694f0c5bd45cae25ba95
R.Murzakov <romuald.murzakov@gmail.com>
2017-06-30 14:36:44 +0100

    Fifth Test done

--- pet_shop.rb
+++ pet_shop.rb
@@ -17,3 +17,29 @@ end
 def increase_pets_sold(shop_name, quantity)
  shop_name[:admin][:pets_sold] += quantity
 end
+
+def stock_count(shop_name)
+  return shop_name[:pets].length
+end
+##### to finish
+# def pets_by_breed(pet_shop, pet)
+#   found_pets = []
+#  for pet in pet_shop[:pets]
+#   found_pets << pet if pet[:breed] = "British Shorthair"
+#  end
+#  return found_pets
+# end
+
+def find_pet_by_name(pet_shop, pet_name)
+result = nil
+  for pet in pet_shop[:pets]
+    if (pet_name == pet[:name])
+      result = pet
+    end
+  end
+  return result
+end
+
+def find_pet_by_name(pet_shop, pet_name)
+
+end
--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -93,7 +93,7 @@ class TestPetShop < Minitest::Test
     cash = total_cash(@pet_shop)
     assert_equal(990, cash)
   end
-  
+
   def test_pets_sold
     sold = pets_sold(@pet_shop)
     assert_equal(0, sold)
@@ -105,11 +105,11 @@ class TestPetShop < Minitest::Test
     assert_equal(2, sold)
   end
 
-  # def test_stock_count
-  #   count = stock_count(@pet_shop)
-  #   assert_equal(6, count)
-  # end
-
+  def test_stock_count
+    count = stock_count(@pet_shop)
+    assert_equal(6, count)
+  end
+######
   # def test_all_pets_by_breed__found
   #   pets = pets_by_breed(@pet_shop, "British Shorthair")
   #   assert_equal(2, pets.count)
@@ -119,22 +119,23 @@ class TestPetShop < Minitest::Test
   #   pets = pets_by_breed(@pet_shop, "Dalmation")
   #   assert_equal(0, pets.count)
   # end
+####to finish
+  def test_find_pet_by_name__returns_pet
+    pet = find_pet_by_name(@pet_shop, "Arthur")
+    puts pet
+    assert_equal("Arthur", pet[:name])
+  end
 
-  # def test_find_pet_by_name__returns_pet
-  #   pet = find_pet_by_name(@pet_shop, "Arthur")
-  #   assert_equal("Arthur", pet[:name])
-  # end
+  def test_find_pet_by_name__returns_nil
+    pet = find_pet_by_name(@pet_shop, "Fred")
+    assert_nil(pet)
+  end
 
-  # def test_find_pet_by_name__returns_nil
-  #   pet = find_pet_by_name(@pet_shop, "Fred")
-  #   assert_nil(pet)
-  # end
-
-  # def test_remove_pet_by_name
-  #   remove_pet_by_name(@pet_shop, "Arthur")
-  #   pet = find_pet_by_name(@pet_shop,"Arthur")
-  #   assert_nil(pet)
-  # end
+  def test_remove_pet_by_name
+    remove_pet_by_name(@pet_shop, "Arthur")
+    pet = find_pet_by_name(@pet_shop,"Arthur")
+    assert_nil(pet)
+  end
 
   # def test_add_pet_to_stock
   #   add_pet_to_stock(@pet_shop, @new_pet)
